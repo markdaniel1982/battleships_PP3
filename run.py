91c85e624cd12e0093902978f981873cd0e59305
Mark Daniel <markdaniel1982@gmail.com>
2023-04-04 07:20:19 +0000

    add user choice input for row and column and convert column letters to upper for validation

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class PlayArea:
+    def __init__(self, board):
+        self.board = board
+
+    def get_let_to_num():
+        let_to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+        return let_to_num
+
+# CODE CREDIT - https://stackoverflow.com/questions/35401019
+# /how-do-i-print-something-underlined-in-python
+    def print_board(self):
+        print("\u0332".join("  A B C D E"))
+        row_number = 1
+        for row in self.board_
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def place_ships(self):
+        for i in range(5):
+            self.Xrow, self.Yclm = random.randit(
+                0, 4), random.randint(0, 4)
+            while self.board[self.Xrow][self.Yclm] == "X":
+                self.Xrow, self.Yclm = random.randint(
+                    0, 4), random.randint(0, 4)
+            self.board[self.Xrow][self.Yclm] = "X"
+        return self.board
+
+    def get_user_input(self):
+        try:
+            Yclm = input("Choose a column (a-e): ").upper()
+            while Yclm not in "ABCDE":
+                print("Not a valid choice. Please select a valid column")
+                Yclm = input("Choose a column (a-e): ").upper()
+
+            Xrow = input("Choose a row (1-5): ")
+            while Xrow not in "12345":
+                print("Not a valid choice. Please select a valid row")
+                Xrow = input("Choose a row (1-5): ")
